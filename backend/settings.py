826b3bf27daa07a9260d147c45810495fcf95f1a
david <dcarte51@msudenver.edu>
2025-02-18 18:32:46 -0700

    rm old secret key

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y$*(*j(x(t^m6g&%9)ec6oe24&tiac_2-mpbs7xeby+xsh91f&'
+# TODO: Add new secret key to GH secrets
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
