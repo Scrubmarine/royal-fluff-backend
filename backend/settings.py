da00582b65d4a79ba547bc3a2f5abab32d1e79d8
david <dcarte51@msudenver.edu>
2025-02-25 09:47:32 -0700

    add env config for local/production

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,10 @@ import environ
 
 env = environ.Env()
 
+if os.environ.get('DJANGO_ENV') != 'production':
+    environ.Env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,8 +93,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
